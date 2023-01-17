bfac2bee0669a68896ccf1dd78ef3c881ea0b082
ofperez-TheOwl <odidiperez@gmail.com>
2023-01-17 23:46:00 +0000

    mandatory tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+8-all
+"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection.
+    """
+    return [doc for doc in mongo_collection.find()]
